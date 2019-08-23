a85f7d698c60ac2492cb3ba1077b804867e5ba64
Poulad <poulad.pld@gmail.com>
2019-08-23 18:53:59 -0400

    PIP :'(

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from jira import JIRA
 
 app = Flask(__name__)
 
@@ -9,4 +10,13 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+
+    jac = JIRA('https://jira.atlassian.com')
+
+    jira = JIRA(options={
+        'server': 'https://poulad.atlassian.net',
+        'basic_auth': ('poulad', 'pass')
+    })
+    iss = jira.issue(1)
+    pass
