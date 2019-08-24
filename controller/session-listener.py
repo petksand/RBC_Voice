bfad4ebf1d88c0ea2d9087c7e1f438a984d9f965
Poulad <poulad.pld@gmail.com>
2019-08-24 07:43:14 -0400

    use java now

--- controller/session-listener.py
+++ controller/session-listener.py
@@ -1,27 +1,23 @@
-from os import environ, path
+import speech_recognition as sr
 
-from pocketsphinx.pocketsphinx import *
-from sphinxbase.sphinxbase import *
 
-MODELDIR = "model"
-DATADIR = "test/data"
+def listen(recog: sr.Recognizer):
+    with sr.Microphone() as source:
+        audio = recog.listen(source=source)
+        return audio
 
-# Create a decoder with certain model
-config = Decoder.default_config()
-config.set_string('-hmm', path.join(MODELDIR, 'en-us/en-us'))
-config.set_string('-lm', path.join(MODELDIR, 'en-us/en-us.lm.bin'))
-config.set_string('-dict', path.join(MODELDIR, 'en-us/cmudict-en-us.dict'))
-decoder = Decoder(config)
 
-# Decode streaming data.
-# decoder = Decoder(config)
-decoder.start_utt()
-stream = open(path.join(DATADIR, 'goforward.raw'), 'rb')
-while True:
-    buf = stream.read(1024)
-    if buf:
-        decoder.process_raw(buf, False, False)
-    else:
-        break
-decoder.end_utt()
-print('Best hypothesis segments: ', [seg.word for seg in decoder.seg()])
+def main():
+    recog = sr.Recognizer()
+    audio = listen(recog)
+    while True:
+        try:
+            recorded: str = recog.recognize_google(audio)
+            print('Recorded: ' + recorded)
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    main()
